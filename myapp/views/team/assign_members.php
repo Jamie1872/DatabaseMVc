a235e7faa6e1a27acd47388d496b4a4016d2d902
jamie <lowjamie8@gmail.com>
2025-07-31 16:41:28 -0400

    make sure club members are of same lcoation when assigning to teams

--- myapp/views/team/assign_members.php
+++ myapp/views/team/assign_members.php
@@ -1,3 +1,8 @@
+<?php
+require_once __DIR__ . '/../../models/ClubMember.php';
+$members = ClubMember::getEligibleMembersForTeam($team['team_id']);
+?>
+
 <h2>Assign Club Member to Team: <?= htmlspecialchars($team['team_name']) ?></h2>
 
 <form action="index.php?action=team_assign_store" method="POST">
