19f30bd533c424db1b2d9429dfeeba7e1a9133ce
jamie <lowjamie8@gmail.com>
2025-07-31 20:23:55 -0400

    go back

--- myapp/views/team/filter_formation_form.php
+++ myapp/views/team/filter_formation_form.php
@@ -15,3 +15,5 @@
 
     <button type="submit">Search</button>
 </form>
+
+<a href="index.php" style="display: inline-block; margin-bottom: 10px; padding: 8px 12px; background-color: #007BFF; color: white; text-decoration: none; border-radius: 4px;">Go Back</a>
