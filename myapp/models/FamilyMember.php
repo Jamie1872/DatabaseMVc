e6a82c429811ed9f112002c31d1b6cce4642d5ac
leiladjef <leiladjeffal07@gmail.com>
2025-08-03 02:25:18 -0400

    query 17
    query 17 adjusted

--- myapp/models/FamilyMember.php
+++ myapp/models/FamilyMember.php
@@ -58,26 +58,26 @@ class FamilyMember {
     }
 
     #query 17
-   public static function getHeadCoachFamilyMembersByLocation($locationId) {
+ public static function getHeadCoachFamilyMembersByLocation($locationId) {
     $db = Database::connect();
 
-    $sql = $sql = "
-   SELECT DISTINCT fm.first_name, fm.last_name, fm.phone_number
-   FROM FamilyMembers fm
-   JOIN ClubMember_Family_Association cmfa ON fm.family_member_id = cmfa.family_member_id
-   JOIN ClubMember_Location_History clh ON cmfa.club_member_id = clh.club_member_id
-   JOIN Personnel p ON p.email = fm.email
-   JOIN Teams t ON t.head_coach_id = p.personnel_id
-   WHERE clh.end_date IS NULL
-   AND clh.location_id = :locId
-   AND t.location_id = :locId
-";
-
+    $sql = "
+        SELECT DISTINCT fm.first_name, fm.last_name, fm.phone_number
+    FROM FamilyMembers fm
+    JOIN ClubMember_Family_Association cmfa ON fm.family_member_id = cmfa.family_member_id
+    JOIN ClubMember_Location_History clh ON cmfa.club_member_id = clh.club_member_id
+    JOIN Personnel p ON p.ssn = fm.ssn  -- More reliable than matching by email
+    JOIN Teams t ON t.head_coach_id = p.personnel_id
+    WHERE clh.end_date IS NULL
+    AND clh.location_id = :locId
+    AND t.location_id = :locId;
+    ";
 
     $stmt = $db->prepare($sql);
     $stmt->execute([':locId' => $locationId]);
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
 
 
 
--- myapp/views/familymember/head_coach_family_list.php
+++ myapp/views/familymember/head_coach_family_list.php
@@ -1,20 +1,11 @@
 <h2>Head Coach Family Members at Selected Location</h2>
 
-<?php if (empty($results)): ?>
-    <p>No head coach family members found for this location.</p>
-<?php else: ?>
-    <table border="1">
-        <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Phone Number</th>
-        </tr>
-        <?php foreach ($results as $row): ?>
-        <tr>
-            <td><?= htmlspecialchars($row['first_name']) ?></td>
-            <td><?= htmlspecialchars($row['last_name']) ?></td>
-            <td><?= htmlspecialchars($row['phone_number']) ?></td>
-        </tr>
+<?php if (!empty($results)): ?>
+    <ul>
+        <?php foreach ($results as $fm): ?>
+            <li><?= htmlspecialchars($fm['first_name'] . ' ' . $fm['last_name'] . ' - ' . $fm['phone_number']) ?></li>
         <?php endforeach; ?>
-    </table>
+    </ul>
+<?php else: ?>
+    <p>No matching family members found.</p>
 <?php endif; ?>
